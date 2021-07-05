193f1b8df3b1760b757fda4a8c3ae936f38390bc
Munawir Islami <munawirrahman@gmail.com>
2021-07-05 21:43:37 +0700

    first commit

---
+++ force-app/main/default/lwc/timer_FSC/timer_FSC.js
@@ -0,0 +1,27 @@
+import { LightningElement,track,api } from 'lwc';
+
+export default class Timer_FSC extends LightningElement {
+
+    @track timeVal = '0:0:0:0';
+    timeIntervalInstance;
+    @api totalMilliseconds = 0;
+
+    connectedCallback() {
+        var parentThis = this;
+
+        // Run timer code in every 100 milliseconds
+        this.timeIntervalInstance = setInterval(function() {
+
+            // Time calculations for hours, minutes, seconds and milliseconds
+            var hours = Math.floor((parentThis.totalMilliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((parentThis.totalMilliseconds % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((parentThis.totalMilliseconds % (1000 * 60)) / 1000);
+            var milliseconds = Math.floor((parentThis.totalMilliseconds % (1000)));
+            
+            // Output the result in the timeVal variable
+            parentThis.timeVal = hours + ":" + minutes + ":" + seconds + ":" + milliseconds;   
+            
+            parentThis.totalMilliseconds += 100;
+        }, 100);
+    }
+}
